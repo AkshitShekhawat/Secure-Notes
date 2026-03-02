4d13d2448fa085efd665fa3c8d8761afbb606c14
akshit-shekhawat <akw17439@gmail.com>
2026-03-02 23:22:38 +0530

    Method Level Security vs RequestMatchers Approach

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.secure.notes.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,6 +27,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
 //    @Bean
@@ -46,7 +48,10 @@ public class SecurityConfig {
 
         @Bean
         SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-            http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+            http.authorizeHttpRequests((requests) ->
+                    requests
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated());
             http.csrf(AbstractHttpConfigurer::disable);
             //http.formLogin(withDefaults());
             http.httpBasic(withDefaults());
