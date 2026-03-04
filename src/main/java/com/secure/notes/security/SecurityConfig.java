e2d4a8c603f34b399f66a7d97a72651a3d6539be
akshit-shekhawat <akw17439@gmail.com>
2026-03-04 13:53:57 +0530

    Configuring CSRF Protection in Spring Security

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import javax.sql.DataSource;
 
@@ -51,11 +52,14 @@ public class SecurityConfig {
 
         @Bean
         SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+            http.csrf(csrf ->
+                    csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+            //http.csrf(AbstractHttpConfigurer::disable);
             http.authorizeHttpRequests((requests) ->
                     requests
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated());
-            http.csrf(AbstractHttpConfigurer::disable);
+                            .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                            .requestMatchers("/api/csrf-token").permitAll()
+                            .anyRequest().authenticated());
             //http.formLogin(withDefaults());
             http.httpBasic(withDefaults());
             return http.build();
